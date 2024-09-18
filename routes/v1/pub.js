6660fcaa31a4dfff0467e50aa674160fb1bb92b7
taissir alem <taissir.alem@gmail.com>
2024-09-18 15:26:23 +0100

    fix

--- routes/v1/pub.js
+++ routes/v1/pub.js
@@ -3,7 +3,7 @@ const pubController = require("../../controllers/Pub");
 const imageUpload = require("../../middlewares/imageUpload");
 const router = Router();
 
-router.post("/create", imageUpload.single("image"), pubController.createPub);
+router.post("/", imageUpload.single("image"), pubController.createPub);
 router.get("/", pubController.getAllPubs);
 router.get("/:id", pubController.getPubById);
 router.put("/:id", imageUpload.single("image"), pubController.updatePub);
